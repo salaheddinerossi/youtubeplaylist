cbaaac92e147d1dba38da44a253c2932cad9b567
Salah <salaheddine.rossi@etu.uae.ac.ma>
2021-12-20 14:07:52 +0100

    thirs commet

--- src/general/Navbar.js
+++ src/general/Navbar.js
@@ -1,11 +1,33 @@
 import Alink from './Alink';
+import { useLocation } from 'react-router-dom';
 function Navbar ()  {
+    const location = useLocation();
+    let isActive = "red font-semibold";
+    let notActive = "text-gray-500";
+    var first , seconde , third ; 
+    if (location.pathname =="/") {
+        first = isActive;
+        seconde = notActive;
+        third = notActive;
+   }
+   if (location.pathname == "/playlist") {
+       first = notActive;
+       seconde = isActive;
+       third = notActive;
+  }
+  if (location.pathname =="/create") {
+       first = notActive;
+       seconde = notActive;
+       third = isActive;
+   }
+
+
     return (
         <div className="bg-white navbarT grid justify-items-center m-0">
             <ul className="flex center">
-                <Alink active="red font-semibold" title="Home" route="/"/>
-                <Alink active="text-gray-500" title="Playlist" route="/playlist" />
-                <Alink active="text-gray-500" title="Create" route="/create"/>
+                <Alink active={first} title="Home" route="/"/>
+                <Alink active={seconde} title="Playlist" route="/playlist" />
+                <Alink active={third} title="Create" route="/create"/>
             </ul>
         </div>
     )
--- src/page3/Ceate.js
+++ src/page3/Ceate.js
@@ -1,18 +1,24 @@
+import { useState } from "react";
 function Create () {
+    const [links,setLinks] = useState([])
+    const addInput = () => {
+        console.log(links)
+    }
     return (
         <div className="container mx-auto mt-5">
-            <h1 className="ml-5">Create your own playlist :</h1>
-            <input className="focus:outline-none input-b mt-3 ml-40" placeholder=" Playlist name " />
-            <input className="focus:outline-none input-b mt-3 ml-40" placeholder=" Add a link here" />
+            <h1 className="ml-5"> Create your own playlist : </h1>
+            <input className="focus:outline-none input-b mt-3 ml-40" placeholder="Playlist name" />
+            <div id="inputs">
+                <input className="focus:outline-none input-b mt-3 ml-40" onChange={(e => links.push(e.target.value))} placeholder="Add a link here" />
+            </div>
             <br/>
-            <button className="bg-gray-800 ml-40 text-white py-1 px-5 rounded-full mt-3">add another input</button>
+            <button onClick={addInput} className="bg-gray-800 ml-40 text-white py-1 px-5 rounded-full mt-3">add another input</button>
             <br/>
             <textarea className="input-b focus:outline-none input-b mt-3 ml-40">
 
             </textarea>
             <br/>
-            <button className="bg-gray-800 ml-40 text-white py-1 px-5 rounded-full my-3">submit</button>
-
+            <button  className="bg-gray-800 ml-40 text-white py-1 px-5 rounded-full my-3">submit</button>
         </div>
     )
 }
