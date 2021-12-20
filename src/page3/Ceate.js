ef08a109effb4e75e89faa6d0192803ce528d276
Salah <salaheddine.rossi@etu.uae.ac.ma>
2021-12-20 18:34:00 +0100

    last commit

--- src/page3/Ceate.js
+++ src/page3/Ceate.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 function Create () {
     const [links,setLinks] = useState([])
+    let i = 0;
+    var arr = [];
     const addInput = () => {
+        links.push(<Input key={i}/>)
+        setLinks([links])
+        i++;    
         console.log(links)
     }
+    function Input (props) {
+        return(
+            <input className="focus:outline-none input-b mt-3 ml-40" value={props.key} placeholder="Add a link here" />
+        )
+    }
     return (
-        <div className="container mx-auto mt-5">
+            <div className="container mx-auto mt-5">
             <h1 className="ml-5"> Create your own playlist : </h1>
             <input className="focus:outline-none input-b mt-3 ml-40" placeholder="Playlist name" />
             <div id="inputs">
-                <input className="focus:outline-none input-b mt-3 ml-40" onChange={(e => links.push(e.target.value))} placeholder="Add a link here" />
+                {links}
             </div>
             <br/>
             <button onClick={addInput} className="bg-gray-800 ml-40 text-white py-1 px-5 rounded-full mt-3">add another input</button>
